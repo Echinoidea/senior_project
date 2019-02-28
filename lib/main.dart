cfb0a4004252cf047654b152929e7cffec307667
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2019-02-28 10:22:18 -0500

    Added functional splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
+import 'pages/splash_screen.dart';
 
 
+void main() {
+  runApp(new MaterialApp(
+    home: new SplashScreen(),
+  ));
+}
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(color: Colors.redAccent),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 50.0,
+                          child: Icon(
+                            Icons.brightness_1,
+                            color: Colors.red,
+                            size: 50.0,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10.0),
+                        ),
+                        Text(
+                          "漢字練習アップリ",
+                          softWrap: true,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        )
+    );
+  }
+}
